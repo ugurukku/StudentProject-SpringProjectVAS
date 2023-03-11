1a02fa2188e3d08ee21585eec2cee2cbafdfdd18
Ugur <ugurkarimli1@gmail.com>
2023-03-11 13:14:59 +0400

    deyishiklik olundu

--- src/main/java/com/example/demospring/DemoSpringApplication.java
+++ src/main/java/com/example/demospring/DemoSpringApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoSpringApplication {
-
+// repository qati elave edildi
     public static void main(String[] args) {
         SpringApplication.run(DemoSpringApplication.class, args);
     }
